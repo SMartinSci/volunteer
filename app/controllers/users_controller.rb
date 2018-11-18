68886814f98953ff452b4be0c481a9512c917f2f
SMartinSci <sm674@cornell.edu>
2018-11-18 07:34:53 -0800

    Adjust users controller routes params and redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,8 @@
 class UsersController < ApplicationController
 
-   get '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
-    erb :'users/show'
-  end
-
   get '/signup' do
     if !logged_in?
-
-      erb :'users/create_user'
-
+      erb :'/users/signup'
     else
       redirect to '/entries'
     end
@@ -17,13 +10,11 @@ class UsersController < ApplicationController
 
   post '/signup' do
     if params[:username] == "" || params[:email] == "" || params[:password] == ""
-
       redirect to '/signup'
     else
       @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
       @user.save
       session[:user_id] = @user.id
-
       redirect to '/entries'
     end
   end
@@ -37,21 +28,23 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect to "/entries"
-    else
-      redirect to '/signup'
-    end
-  end
+    @user = User.find_by(:email => params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id	
+      redirect to "/entries"	    
+    else	
+      redirect "/users/#{@user.id}"
+    end	
+  end	  
 
   get '/logout' do
     if logged_in?
-      session.destroy
-      redirect to '/login'
-    else
       redirect to '/'
     end
   end
-end
+
+  get '/users/:id' do
+      @user = User.find_by_id(params[:id])
+          erb :'/users/show'
+    end
+  end	
