f9c8fb85f218a8c25ba6b8e764309d23c0e1ebdb
SMartinSci <sm674@cornell.edu>
2018-12-04 11:37:45 -0800

    Remove index get request method from entries controller, create all_users view and route

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,14 +1,14 @@
 class EntriesController < ApplicationController
 
   get '/entries' do
-    if logged_in?
-      @user = current_user
+    # if logged_in?
+      # @user = current_user
       @entries = Entry.all
-      erb :'entries/new'
-    else
-      flash[:failure] = "Signup or login to access your volunteer entries."
-      redirect '/'
-    end
+      erb :'entries/index'
+    # else
+    #   flash[:failure] = "Signup or login to access your volunteer entries."
+    #   redirect '/'
+    # end
   end
 
   get '/entries/new' do
@@ -34,10 +34,16 @@ class EntriesController < ApplicationController
     end
   end
 
-  get '/entries/index' do
-    @entry = Entry.find_by_id(params[:id])
+  # get '/entries/index' do
+  #   @entry = Entry.find_by_id(params[:id])
+  #   @entries = Entry.all
+  #   erb :'/entries/index'
+  # end
+
+  get '/entries/all_users' do
+    # @entry = Entry.find_by_id(params[:id])
     @entries = Entry.all
-    erb :'/entries/index'
+    erb :'/entries/all_users'
   end
 
   get '/entries/:id' do
